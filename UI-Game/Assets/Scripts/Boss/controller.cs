1f0603f3a6a6d61a3b3651344383adb9276da3c6
Chad18Z <credeurchad@gmail.com>
2019-11-16 09:43:37 -0700

    Boss prefab imported into scene

---
+++ UI-Game/Assets/Scripts/Boss/controller.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controller : MonoBehaviour
+{
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        animator.SetBool("isWalking", false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            animator.SetBool("isWalking", true);
+        }
+        else if (Input.GetKeyUp(KeyCode.D))
+        {
+            animator.SetBool("isWalking", false);
+        }
+    }
+
+    public void OnQuit()
+    {
+        Application.Quit();
+    }
+}
