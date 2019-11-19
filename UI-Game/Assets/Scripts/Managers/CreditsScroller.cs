3733a72430a9342356f4b9a753642e46198070df
Chad18Z <credeurchad@gmail.com>
2019-11-19 16:36:36 -0700

    Boss scene nearly complete, only need to add transition to credits (and crowd noises in the club)

---
+++ UI-Game/Assets/Scripts/Managers/CreditsScroller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreditsScroller : MonoBehaviour
+{
+    [SerializeField]
+    GameObject creditsPanel;
+
+    Animator anim;
+    
+    private void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
