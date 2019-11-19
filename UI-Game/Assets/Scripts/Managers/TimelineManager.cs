360dfd3c2e33700b06dc1b8f248e137653d1e8de
Chad18Z <credeurchad@gmail.com>
2019-11-19 09:30:30 -0700

    Adding ghost animations to boss level

---
+++ UI-Game/Assets/Scripts/Managers/TimelineManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimelineManager : MonoBehaviour
+{
+    GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    public void FlipCharacter()
+    {
+        Vector3 scale = player.transform.localScale;
+        scale.x *= -1;
+        player.transform.localScale = new Vector3(scale.x, 1, 1);
+    }
+}
