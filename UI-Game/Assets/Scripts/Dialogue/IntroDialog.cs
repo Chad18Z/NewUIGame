3172f2a5410fd1f31213d49eee0b373882097ffd
Chad18Z <credeurchad@gmail.com>
2019-11-19 21:36:57 -0700

    Intro scene started

---
+++ UI-Game/Assets/Scripts/Dialogue/IntroDialog.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroDialog : MonoBehaviour
+{
+    int index = 0;
+    Teletype teletype;
+    string[] text =
+    {
+        "Wha- Where am I?",
+        "Be careful, those purple cookies are chocked full O' shrooms",
+        "...I always knew morale would improve after I become the boss",
+        "Anyhoo, come back inside, we're gonna hand out annual awards",
+        "...and take that sheet off yer head, you look ridiculous!"
+    };
+    // Start is called before the first frame update
+    void Start()
+    {
+        teletype = GameObject.FindGameObjectWithTag("teletype").GetComponent<Teletype>();
+    }
+
+    public void HandleSignal()
+    {
+        //Debug.Log("Signal received");
+        if (index < text.Length)
+        {
+            ShowString(text[index]);
+            index++;
+        }
+        else
+        {
+            Debug.Log("Out of range");
+        }
+    }
+
+    void ShowString(string message)
+    {
+        teletype.ShowMessage(message);
+    }
+}
